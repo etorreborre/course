c2e574250490d18e0cc698ee9ff21a78069bdf72
Eric Torreborre <etorreborre@yahoo.com>
2014-10-14 17:47:24 +1100

    got a version of move which checks if the game is finished or not using GADTs

--- src/TicTacToe.hs
+++ src/TicTacToe.hs
@@ -1,25 +1,52 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE Rank2Types #-}
 module TicTacToe where
 
--- `move`: takes a tic-tac-toe board and position and moves to that position (if not occupied) returning a new board. This function can only be called on a board that is empty or in-play. Calling `move` on a game board that is finished is a *compile-time type error*.
+import Prelude
+import Data.List
+
+-- `move`: takes a tic-tac-toe board and position and moves to that position (if not occupied) returning a new board.
+-- This function can only be called on a board that is empty or in-play. Calling `move` on a game board that is finished is a *compile-time type error*.
 
 data Position =
  NE  | NC  | NW  |
  CE  | CC  | CW  |
- SE  | SC  | SW
+ SE  | SC  | SW    deriving (Eq,Show)
 
-data BoardCell = BoardCell Position Move
+data BoardCell = BoardCell Position Move deriving (Eq,Show)
 
-data Cell = Empty | C Move
+data Cell = Empty | C Move deriving (Eq,Show)
 
-data Move = X | O
+data Move = X | O deriving (Eq,Show)
 
-data Board =
-	Cells [BoardCell]
+data Board a where
+  Unfinished :: [BoardCell] -> Board UnfinishedBoard
+  Finished   :: [BoardCell] -> Board FinishedBoard
 
-data NotFull = NotFull
-data Full = Full
+cells :: forall a. Board a -> [BoardCell]
+cells (Unfinished cs) = cs
+cells (Finished cs)   = cs
 
-move :: Board -> Position -> Move -> Board
-move b c m = undefined
+-- Game state: unfinished, finished
+data UnfinishedBoard
+data FinishedBoard
 
-undefined = undefined
+data BoardTry = forall a. BoardTry (Board a) | Failed
+
+instance Show BoardTry where
+  show (BoardTry board) = show $ cells board
+  show Failed           = show "Failed move"
+
+move :: Board UnfinishedBoard -> Position -> Move -> BoardTry
+move (Unfinished cells) p m =
+  case find (contains p) cells of
+    Just (BoardCell p1 m) -> Failed
+    Nothing               -> if length cells == 9 then BoardTry $ Finished   ((BoardCell p m) : cells)
+                             else                       BoardTry $ Unfinished ((BoardCell p m) : cells)
+
+move' :: BoardTry -> Position -> Move -> BoardTry
+move' (BoardTry (Unfinished cells)) p m = move (Unfinished cells) p m
+move' _ _ _       = Failed
+
+contains :: Position -> BoardCell -> Bool
+contains p1 (BoardCell p2 _) = p1 == p2
